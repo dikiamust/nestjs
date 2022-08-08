22ae1725d91f33ed593d565bee7413570d13f8d1
dikiamust <agungmustaqimdiki@gmail.com>
2022-08-08 17:10:14 +0700

    add port on env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
 
@@ -17,6 +20,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = configService.get('PORT') | 3000;
+
+  await app.listen(port);
 }
 bootstrap();
