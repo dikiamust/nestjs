f0bc010775b661985047aba85bd7c6523be4f4b6
dikiamust <agungmustaqimdiki@gmail.com>
2022-08-11 20:37:02 +0700

    master branch as main branch

--- src/database/entities/role.entity.ts
+++ src/database/entities/role.entity.ts
@@ -2,15 +2,11 @@ import {
     Entity,
     Column,
     PrimaryGeneratedColumn,
-    ManyToOne,
 } from "typeorm";
-import { User } from "./user.entity";
 
 @Entity()
 export class Role {
     @PrimaryGeneratedColumn()
-    // @ManyToOne(() => User, (user) => user.roleId)
-    // user: User
     id: number
 
     @Column({ nullable: false, unique: true })
